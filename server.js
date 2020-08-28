4e9623ac7ed1d6cc1dff9784f15b07297e501be7
Gedalya Krycer <gedalya@krycer.com>
2020-08-28 15:36:21 -0700

    Added base GET API route to display data.

--- server.js
+++ server.js
@@ -43,7 +43,6 @@ app.use('/', htmlRouter);
 // LISTENER
 // The below code effectively "starts" our server
 // =============================================================================
-
 app.listen(PORT, function () {
     console.log("App listening on PORT: " + PORT);
 });
