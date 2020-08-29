7906fba0c044c8f5f88ec7a6b2e7f5ee9beeb7af
Gedalya Krycer <gedalya@krycer.com>
2020-08-29 09:30:23 -0700

    Added api

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,9 +1,5 @@
 const express = require('express');
-// ===============================================================================
-// LOAD DATA
-// We are linking our routes to a series of "data" sources.
-// These data sources hold arrays of information on table-data, waitinglist, etc.
-// ===============================================================================
+const fs = require('fs');
 const notesData = require("../db/db.json");
 
 
@@ -20,28 +16,24 @@ const router = express.Router();
 // ---------------------------------------------------------------------------
 
 router.get("/notes", function (req, res) {
-  res.json(notesData);
+    res.json(notesData);
 });
 
-
-
-// API POST Requests
-// Below code handles when a user submits a form and thus submits data to the server.
-// In each of the below cases, when a user submits form data (a JSON object)
-// ...the JSON is pushed to the appropriate JavaScript array
-// (ex. User fills out a reservation request... this data is then sent to the server...
-// Then the server saves the data to the tableData array)
-// ---------------------------------------------------------------------------
 
 router.post("/notes", function (req, res) {
-  // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-  // It will do this by sending out the value "true" have a table
-  // req.body is available since we're using the body parsing middleware
+    console.log(req.body)
 
-    tableData.push(req.body);
-    // res.json(true);
+    data = JSON.stringify(req.body, null, 2);
+
+    fs.appendFile('./db/db.json', data, (err) => {
+        if (err) throw err;
+        console.log('Data written to file');
+    });
 
 });
+
+
+
 
 // ---------------------------------------------------------------------------
 // I added this below code so you could clear out the table while working with the functionality.
